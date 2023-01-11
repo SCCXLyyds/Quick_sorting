3a5940bad1d2218441821f7b5881fa406325b528
SCCXLyyds <121226086+SCCXLyyds@users.noreply.github.com>
2023-01-11 21:19:38 +0800

    Update 快速排序.cpp

--- 快速排序.cpp
+++ 快速排序.cpp
@@ -1,37 +1,60 @@
+//快速排序： 
+//#include <iostream>
+//using namespace std;
+//const int N=1e6+10;
+//int a[N],n,l,j;
+//void quick_sort(int a[],int start,int end){
+//	if(start>=end){
+//		return;
+//	} 
+//	int temp=a[(start+end)/2];
+//	l=start-1;j=end+1;
+//	while(l<j){
+//		do l++;while(a[l]<temp);
+//		do j--;while(a[j]>temp);
+//		if(l<j){
+//			swap(a[l],a[j]);
+//		}
+//	}
+//	quick_sort(a,start,j);
+//	quick_sort(a,j+1,end);
+//}
+//int main()
+//{
+//	scanf("%d",&n);
+//	for(int i=0;i<n;i++){
+//		scanf("%d",&a[i]);
+//	}
+//	quick_sort(a,0,n-1);
+//	for(int i=0;i<n;i++){
+//		printf("%d ",a[i]);
+//	}
+//	return 0;
+//}
+//第 k 小的数 
 #include <iostream>
 using namespace std;
-// const int N=9;
-void quick_sort(int a[],int begin,int end){
-	if(begin>end){
-		return;
+const int N=1e6+10;
+int a[N],n,k;
+int quick_find(int a[],int start,int end,int k) 
+{
+	if(start>=end) return a[end];
+	int temp=a[(start+end)/2];
+	int l=start-1,j=end+1;
+	while(l<j){
+		do l++;while(a[l]<temp);
+		do j--;while(a[j]>temp);
+		if(l<j) swap(a[l],a[j]);
 	}
-	int temp=a[begin];  //方便后面 temp和i=j时对应的值交换
-	int i=begin;
-	int j=end;
-	while(i != j){
-		while(a[j]>=temp && j>i){
-			j--;
-		}
-		while(a[i]<=temp && j>i){
-			i++;
-		}
-		if(j>i){
-			int t=a[i];
-			a[i]=a[j];
-			a[j]=t;
-		}
-	} 
-	a[begin]=a[i];
-	a[i]=temp;
-	quick_sort(a,begin,i-1);
-	quick_sort(a,i+1,end); 
+	if(k<=j+1-start) return quick_find(a,start,j,k);
+	else return quick_find(a,j+1,end,k-j-1+start);
 }
 int main()
-{   
-	int a[9]={6,5,7,1,12,4,0,9,8};
-	quick_sort(a,0,8);
-	for(int i=0;i<9;i++){
-		cout<<a[i]<<" ";
+{
+	cin>>n>>k;
+	for(int i=0;i<n;i++){
+		cin>>a[i];
 	}
+	cout<<quick_find(a,0,n-1,k)<<endl;
 	return 0;
 }
